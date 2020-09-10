a0630dd941104dc3de08dee8d13e90c22499b4de
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-09-10 17:12:19 +0900

    edit

---
+++ unit2/code/code/samechar.cpp
@@ -0,0 +1,11 @@
+//
+//  samechar.cpp
+//  code
+//
+//  Created by Ryota Eisaki on 2020/09/10.
+//  Copyright © 2020 Ryota Eisaki. All rights reserved.
+//
+
+#include "samechar.hpp"
+
+
---
+++ unit2/code/code/samechar.hpp
@@ -0,0 +1,14 @@
+//
+//  samechar.hpp
+//  code
+//
+//  Created by Ryota Eisaki on 2020/09/10.
+//  Copyright © 2020 Ryota Eisaki. All rights reserved.
+//
+
+#ifndef samechar_hpp
+#define samechar_hpp
+
+#include <stdio.h>
+
+#endif /* samechar_hpp */
