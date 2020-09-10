83789db58ad01b96c670c5ccca4159edff39ce77
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-09-10 15:36:48 +0900

    edit

--- unit2/code/code/point.cpp
+++ unit2/code/code/point.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "point.hpp"
+
+int N = 6;
+int R = 10;
+int X[6] = {1, 7, 15, 20, 30, 50};
+
+void solve() {
+    sort(X, X+N);
+    
+    int i = 0; ans = 0;
+    while (i < N) {
+        int s = X[i++];
+        while (i < N && X[i] <= s + R) i++;
+        
+        int p = X[i - 1];
+        while (i < N && X[i] <= p + R) i++;
+        
+        ans++;
+    }
+    
+    printf("%d\n", ans);
+}
+
