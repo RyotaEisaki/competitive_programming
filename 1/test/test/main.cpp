c360e5e1b7c1687af99efe7c45b1b9df5d846db2
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2021-01-01 17:04:17 +0900

    setting

---
+++ 1/test/test/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    // input from txt （提出時にこの箇所は削除すること）
+    std::ifstream in("input.txt");
+    std::cin.rdbuf(in.rdbuf());
+
+    // cinを高速にするためのおまじない
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    /////////////////////
+    // Write code below /
+    /////////////////////
+
+
+    return 0;
+}
