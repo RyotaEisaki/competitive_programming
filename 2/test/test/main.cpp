711705084f22ba8d0557f3b5d50057d2fb97306f
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2021-01-02 17:27:23 +0900

    edit

--- 2/test/test/main.cpp
+++ 2/test/test/main.cpp
@@ -10,8 +10,13 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 
-//    solve_partial_sum();
-    solve_lake_counting();
+    // input from txt （提出時にこの箇所は削除すること）
+        std::ifstream in("input.txt");
+        std::cin.rdbuf(in.rdbuf());
+
+        // cinを高速にするためのおまじない
+        cin.tie(0);
+        ios::sync_with_stdio(false);
 
     return 0;
 }
